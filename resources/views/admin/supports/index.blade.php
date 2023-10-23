5b3b807e0827e1fda23eff75234fe718e6fd90de
André Luiz <andreluiz.me@gmail.com>
2023-10-23 16:43:43 -0400

    chore: alteração index.blade

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,2 +1,22 @@
 <h1>Listagem</h1>
 <p>Listagem suportes</p>
+
+<table>
+    <thead>
+    <th>assuntos</th>
+    <th>status</th>
+    <th>descrição</th>
+    <th></th>
+    </thead>
+    <tbody>
+    @foreach($supports as $support)
+        <tr>
+            <td>{{ $support -> subject }}</td>
+            <td>{{ $support -> status }}</td>
+            <td>{{ $support -> body }}</td>
+            <td></td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
+
