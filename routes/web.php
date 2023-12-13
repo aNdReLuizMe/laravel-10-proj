ff2a2e668a1783d3043e9fd27cd4a7f9cc412eb7
André Luiz <andreluiz.me@gmail.com>
2023-12-13 17:50:08 -0400

    chore: Criando rotas

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+
 Route::get('/contato', [SiteController::class, 'contact']);
 
 Route::get('/', function () {
